117cf05f50e1ab6c036fd783a58f4a33f7014068
MostafaKadry <mostafakadry806@gmail.com>
2023-02-21 14:31:23 +0200

    first commit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 let database;
-const url = `mongodb+srv://mostafa:test123@thobdb.8uowv.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${process.env.DBN}:${process.env.DBP}@thobdb.8uowv.mongodb.net/?retryWrites=true&w=majority`;
 module.exports = {
   connectToDb: (cb) => {
     MongoClient.connect(url)
